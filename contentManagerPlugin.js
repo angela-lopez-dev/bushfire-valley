627297d47ba8d3a8437188162022fdb16fd2ee09
Angela Lopez <angela.lopez.etu@univ-lille.fr>
2020-12-25 14:15:56 +0100

    constructor content Manager

--- contentManagerPlugin.js
+++ contentManagerPlugin.js
@@ -11,6 +11,9 @@ class ContentManagerPlugin extends Phaser.Plugins.ScenePlugin{
      * @param {*} contentType VIDEO, DIALOG, SOUND
      */
     constructor(scene, pluginManager, contentType){
-
+        super(scne, pluginManager);
+        this.eventEmitter = this.scene.sys.events;
+        this.eventEmitter.on('shutdown', this.shutdown, this);
+        this.eventEmitter.on('destroy', this.destroy, this);
     }
 }

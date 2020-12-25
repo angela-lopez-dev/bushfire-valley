0ebd69e24d3bf447d6a4f678f9f687f7f3717895
Angela Lopez <angela.lopez.etu@univ-lille.fr>
2020-12-25 14:14:57 +0100

    content Manager

---
+++ contentManagerPlugin.js
@@ -0,0 +1,16 @@
+const contentType = Object.freeze({
+    VIDEO : 0,
+    DIALOG : 1,
+    SOUND : 2
+});
+class ContentManagerPlugin extends Phaser.Plugins.ScenePlugin{
+    /**
+     * 
+     * @param {*} scene 
+     * @param {*} pluginManager 
+     * @param {*} contentType VIDEO, DIALOG, SOUND
+     */
+    constructor(scene, pluginManager, contentType){
+
+    }
+}
